0a94d97cdd675bc875c726097ae367cdc08a67fd
Jim Herbert <jh00648@my.westga.edu>
2024-06-12 18:53:33 -0400

    Modified the default tabs to allow for closing using a small X icon

--- MainWindow.cs
+++ MainWindow.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BBB
 {
     public partial class MainWindow : Form
     {
+        private const int TAB_LEADING_SPACE = 8;
+        private const int TAB_TRAILING_SPACE = 16;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -21,6 +18,55 @@ namespace BBB
         private void RunTestToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            this.AdjustTabSizes();
+        }
+
+        private void MainWindowTabControl_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            e.Graphics.DrawString("x", e.Font, Brushes.Black, e.Bounds.Right - TAB_TRAILING_SPACE, e.Bounds.Top + 4);
+            e.Graphics.DrawString(this.MainWindowTabControl.TabPages[e.Index].Text, 
+                e.Font, Brushes.Black, e.Bounds.Left + TAB_LEADING_SPACE, e.Bounds.Top + 4);
+            e.DrawFocusRectangle();
+        }
+
+        private void AdjustTabSizes()
+        {
+            MainWindowTabControl.DrawMode = TabDrawMode.OwnerDrawFixed;
+            MainWindowTabControl.SizeMode = TabSizeMode.Fixed;
+
+            int tabLength = MainWindowTabControl.ItemSize.Width;
+
+            for (int i = 0; i < MainWindowTabControl.TabPages.Count; i++)
+            {
+                TabPage currentPage = MainWindowTabControl.TabPages[i];
+                int currentTabLength = TextRenderer.MeasureText(currentPage.Text, MainWindowTabControl.Font).Width;
+                currentTabLength += TAB_LEADING_SPACE + TAB_TRAILING_SPACE;
+
+                if (currentTabLength > tabLength)
+                {
+                    tabLength = currentTabLength;
+                }
+            }
+
+            MainWindowTabControl.ItemSize = new Size(tabLength, MainWindowTabControl.ItemSize.Height);
+        }
+
+        private void MainWindowTabControl_MouseUp(object sender, MouseEventArgs e)
+        {
+            int tabIndex = MainWindowTabControl.SelectedIndex;
+
+            Rectangle closeButtonBounds = new Rectangle(
+                MainWindowTabControl.GetTabRect(tabIndex).Right - TAB_TRAILING_SPACE,
+                MainWindowTabControl.GetTabRect(tabIndex).Top, TAB_TRAILING_SPACE, MainWindowTabControl.ItemSize.Height);
+
+            if (closeButtonBounds.Contains(e.Location))
+            {
+                MainWindowTabControl.Controls.Remove(MainWindowTabControl.TabPages[tabIndex]);
+            }
         }
     }
 }
