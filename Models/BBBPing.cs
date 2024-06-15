3d2161fd776e30b84ae63ad1162b7c69360b97f5
Davide Gaetano <dg00185@my.westga.edu>
2024-06-15 14:15:36 -0400

    BBBping: update comments

--- Models/BBBPing.cs
+++ Models/BBBPing.cs
@@ -10,7 +10,9 @@ using System.Threading.Tasks;
 
 namespace BBB.Models
 {
-
+/// <summary>
+/// BBBPing (because Ping exists in C#) is a class that provides a simple object with timestamps and data that is useful for testing basic REST functions
+/// </summary>
     internal class BBBPing
     {
         [JsonInclude]
@@ -20,12 +22,21 @@ namespace BBB.Models
         [JsonInclude]
         public String testData { get; private set; }
 
+        /// <summary>
+        /// Empty Constructor
+        /// </summary>
         public BBBPing()
         {
             this.sendTime = DateTime.MinValue;
             this.returnTime = DateTime.MinValue;
             this.testData = "";
         }
+        /// <summary>
+        /// Full Constructor
+        /// </summary>
+        /// <param name="s">send time</param>
+        /// <param name="r">return time</param>
+        /// <param name="testData">test data</param>
         public BBBPing(DateTime s, DateTime r, string testData)
         {
             this.sendTime = s;
@@ -33,11 +44,19 @@ namespace BBB.Models
             this.testData = testData;
         }
 
+        /// <summary>
+        /// Convert current Object into a JSON representation.
+        /// </summary>
+        /// <returns></returns>
         public string ToJSON()
         {
             return JsonSerializer.Serialize(this);
         }
 
+        /// <summary>
+        /// Set the values of the current Object based on JSON string 
+        /// </summary>
+        /// <param name="json"></param>
         public void SetAsJSON(string json)
         {
             Debug.WriteLine($"json is {json}");
@@ -47,6 +66,12 @@ namespace BBB.Models
             this.returnTime = temp.returnTime;
             this.testData = temp.testData;
         }
+
+        /// <summary>
+        /// Create a new Object from JSON string
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns></returns>
         static public BBBPing FromJSON(string json)
         {
             try
