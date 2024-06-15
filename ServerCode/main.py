34d2336bb55af78cba48f1c55eec24441f24e981
Rob Kramlich <kramlich.rob@mail.fcboe.org>
2024-06-15 16:56:11 -0400

    added main.py with inital setup of modules for API and MySQL communication

---
+++ ServerCode/main.py
@@ -0,0 +1,17 @@
+﻿#!/usr/bin/python3
+
+from flask import Flask, request, jsonify
+import json
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+from types import SimpleNamespace
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://rk00057:xgfUVvOewU@localhost/rk00057'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
