1d982b3b331ce3ed98e0ccfa93228122e643a8c2
Davide Gaetano <dg00185@my.westga.edu>
2024-06-19 12:40:48 -0400

    Blank AuthController

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using BBB.ClientRESTHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBB.Controllers
+{
+    internal class AuthController
+    {
+        private RESTClient<ValidateClientMessage, ValidateServerMessage> restClient;
+        
+        public AuthController() {
+            this.restClient = new RESTClient<ValidateClientMessage, ValidateServerMessage>();
+        }
+
+
+    }
+}
