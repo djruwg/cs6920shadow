de03cb6d4c2ad57a467448ae9db2c371a7346f57
Jim Herbert <jh00648@my.westga.edu>
2024-06-18 22:02:47 -0400

    Added unit tests for settings controller

---
+++ UnitTesting/SettingsControllerrTest.cs
@@ -0,0 +1,16 @@
+﻿using BBB.Controllers;
+
+namespace UnitTesting
+{
+    [TestClass]
+    internal class SettingsControllerrTest
+    {
+        [TestMethod]
+        public void TestGetDefaultURL()
+        {
+            SettingsController settings = new SettingsController();
+
+            Assert.AreEqual("https://westga.edu/", settings.startupURL);
+        }
+    }
+}
