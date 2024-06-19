3c0e13626c5f50cef3ca66354bfb3daa651e8b84
Davide Gaetano <dg00185@my.westga.edu>
2024-06-18 21:34:53 -0400

    Add test for Settings

---
+++ UnitTesting/SettingsTest.cs
@@ -0,0 +1,23 @@
+﻿using BBB.DAL;
+using BBB.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class SettingsTest
+    {
+        [TestMethod]
+        public void TestGetSettings()
+        {
+            SettingsDAL settingsDAL = new SettingsDAL();
+            Settings settings = settingsDAL.GetSettings();
+            Assert.AreEqual(settings.starturl, "http://westga.edu/");
+        }
+    }
+}
