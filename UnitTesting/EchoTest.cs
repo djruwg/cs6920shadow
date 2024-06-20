966a0b22c7171d0930a76fda6ea98fa00ac61f81
Davide Gaetano <dg00185@my.westga.edu>
2024-06-19 23:02:54 -0400

    Implement Tests for Echo class

---
+++ UnitTesting/EchoTest.cs
@@ -0,0 +1,39 @@
+﻿using BBB.Interface;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using BBB.ClientRESTHelpers;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class EchoTest
+    {
+        [TestMethod]
+        public void TestFromJSON()
+        {
+            Echo echo = Echo.FromJSON("{\"message\": \"test message\"}");
+            Assert.AreEqual("test message", echo.message);
+        }
+
+        [TestMethod]
+        public void TestToJSON()
+        {
+            Echo echo = Echo.FromJSON("{\"message\": \"test message\"}");
+            string json = echo.ToJSON();
+            Assert.AreEqual("{\"message\":\"test message\"}", json);
+        }
+
+        [TestMethod]
+        public void TestSetAsJSON()
+        {
+            Echo echo = new Echo();
+            echo.SetAsJSON("{\"message\": \"test message\"}");
+            Assert.AreEqual("test message", echo.message);
+        }
+    }
+}
