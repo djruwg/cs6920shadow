d50ba5292f3377796faee26d63c4ab28adf64493
Davide Gaetano <dg00185@my.westga.edu>
2024-06-19 22:32:40 -0400

    Tests for RESTClient

---
+++ ClientRESTHelpers/Echo.cs
@@ -0,0 +1,56 @@
+﻿using BBB.Interface;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BBB.ClientRESTHelpers
+{
+    internal class Echo : BBBSerializableObject
+    {
+        [JsonInclude]
+
+        public string message { get; private set; }
+        /// <summary>
+        /// Convert current Object into a JSON representation.
+        /// </summary>
+        /// <returns></returns>
+        public string ToJSON()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        /// <summary>
+        /// Set the values of the current Object based on JSON string 
+        /// </summary>
+        /// <param name="json"></param>
+        public void SetAsJSON(string json)
+        {
+            Echo temp = Echo.FromJSON(json);
+            this.message = temp.message;
+        }
+
+        /// <summary>
+        /// Create a new Object from JSON string
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        static public Echo FromJSON(string json)
+        {
+            try
+            {
+                return JsonSerializer.Deserialize<Echo>(json);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($" Deserialize got {ex.Message}");
+                return new Echo();
+            }
+        }
+    }
+
+}
---
+++ UnitTesting/RESTClientTest.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BBB.ClientRESTHelpers;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class RESTClientTest
+    {
+        private static RESTClient<Echo, Echo> _restClient;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            _restClient = new RESTClient<Echo, Echo> ();
+        }
+        //[ClassCleanup]
+        //[TestInitialize]
+        //[TestCleanup]
+
+        [TestMethod]
+        public void TestGetObjectAsyncGoodEndpoint()
+        {
+            RestServiceMasterEndpointRoot.Instance.Mode = "TEST";
+            RESTClientReturnData<Echo> wrapper = Task.Run(() => _restClient.GetObjectAsync("/echo")).Result;
+            Assert.IsTrue(wrapper.success, "testing wrapper.success");
+            Assert.IsTrue(wrapper.containsData, "testing wrapper.containsData");
+            Assert.AreEqual("get /echo",wrapper.obj.message);
+        }
+
+        [TestMethod]
+        public void TestPutObjectAsyncGoodEndpoint()
+        {
+
+            RestServiceMasterEndpointRoot.Instance.Mode = "TEST";
+            Echo echo = Echo.FromJSON("{ \"message\": \"put /echo\"}");
+            RESTClientReturnData<Echo> wrapper = Task.Run(() => _restClient.PutObjectAsync("/echo", echo)).Result;
+            Assert.IsTrue(wrapper.success, "testing wrapper.success");
+            Assert.IsTrue(wrapper.containsData, "testing wrapper.containsData");
+            Assert.AreEqual("put /echo", wrapper.obj.message);
+        }
+
+        [TestMethod]
+        public void TestPostObjectAsyncGoodEndpoint()
+        {
+            RestServiceMasterEndpointRoot.Instance.Mode = "TEST";
+            Echo echo = Echo.FromJSON("{ \"message\": \"post /echo\"}");
+            RESTClientReturnData<Echo> wrapper = Task.Run(() => _restClient.PostObjectAsync("/echo", echo)).Result;
+            Assert.IsTrue(wrapper.success, "testing wrapper.success");
+            Assert.IsTrue(wrapper.containsData, "testing wrapper.containsData");
+            Assert.AreEqual("post /echo", wrapper.obj.message);
+        }
+
+        [TestMethod]
+        public void TestDeleteObjectAsyncGoodEndpoint()
+        {
+            RestServiceMasterEndpointRoot.Instance.Mode = "TEST";
+            RESTClientReturnData<Echo> wrapper = Task.Run(() => _restClient.DeleteObjectAsync("/echo")).Result;
+            Assert.IsTrue(wrapper.success, "testing wrapper.success");
+            Assert.IsFalse(wrapper.containsData, "testing wrapper.containsData");
+            Assert.IsNull(wrapper.obj);
+        }
+
+        [TestMethod]
+        public void TestGetObjectAsyncBadEndpoint()
+        {
+            RestServiceMasterEndpointRoot.Instance.Mode = "TESTBAD";
+            RESTClientReturnData<Echo> wrapper = Task.Run(() => _restClient.GetObjectAsync("/echo")).Result;
+            Assert.IsFalse(wrapper.success, "testing wrapper.success");
+            Assert.IsFalse(wrapper.containsData, "testing wrapper.containsData");
+            Assert.IsNull(wrapper.obj);
+        }
+
+        [TestMethod]
+        public void TestPutObjectAsyncBadEndpoint()
+        {
+            RestServiceMasterEndpointRoot.Instance.Mode = "TESTBAD";
+            Echo echo = Echo.FromJSON("{ \"message\": \"put /echo\"}");
+            RESTClientReturnData<Echo> wrapper = Task.Run(() => _restClient.PutObjectAsync("/echo", echo)).Result;
+            Assert.IsFalse(wrapper.success, "testing wrapper.success");
+            Assert.IsFalse(wrapper.containsData, "testing wrapper.containsData");
+            Assert.IsNull(wrapper.obj);
+        }
+
+        [TestMethod]
+        public void TestPostObjectAsyncBadEndpoint()
+        {
+            RestServiceMasterEndpointRoot.Instance.Mode = "TESTBAD";
+            Echo echo = Echo.FromJSON("{ \"message\": \"post /echo\"}");
+            RESTClientReturnData<Echo> wrapper = Task.Run(() => _restClient.PostObjectAsync("/echo", echo)).Result;
+            Assert.IsFalse(wrapper.success, "testing wrapper.success");
+            Assert.IsFalse(wrapper.containsData, "testing wrapper.containsData");
+            Assert.IsNull(wrapper.obj);
+        }
+
+        [TestMethod]
+        public void TestDeleteObjectAsyncBadEndpoint()
+        {
+            RestServiceMasterEndpointRoot.Instance.Mode = "TESTBAD";
+            RESTClientReturnData<Echo> wrapper = Task.Run(() => _restClient.DeleteObjectAsync("/echo")).Result;
+            Assert.IsFalse(wrapper.success, "testing wrapper.success");
+            Assert.IsFalse(wrapper.containsData, "testing wrapper.containsData");
+            Assert.IsNull(wrapper.obj);
+        }
+
+        //public async Task<RESTClientReturnData<TOUT>> GetObjectAsync(string restEndpoint)
+        //public async Task<RESTClientReturnData<TOUT>> PutObjectAsync(string restEndpoint, TIN obj)
+        //public async Task<RESTClientReturnData<TOUT>> PostObjectAsync(string restEndpoint, TIN obj)
+        //public async Task<RESTClientReturnData<TOUT>> DeleteObjectAsync(string restEndpoint)
+    }
+}
